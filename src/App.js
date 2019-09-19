1ff0ccd3efbeb740ba3b79cb011a097c695f93c0
QzarD <a---angel@mail.ru>
2019-09-19 16:55:43 +0700

    add standart structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="inner">
+          <div className="menu">
+            <button className="c-hamburger c-hamburger-line">
+              <span>Menu</span>
+            </button>
+          </div>
+          <div className="logo">
+            <img src="./../public/logo192.png" alt="logo192"/>
+          </div>
+          <div className="nameSite">
+            <span>YouListing</span>
+          </div>
+          <div className="search">
+            <input type="text"/>
+          </div>
+          <div className="button">
+            <button>+ Add Listing</button>
+          </div>
+          <div className="icoHome">
+
+          </div>
+          <div className="icoMessage"></div>
+          <div className="icoBell"></div>
+          <div className="signIn"></div>
+        </div>
       </header>
     </div>
   );
