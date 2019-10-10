c287e8ac3b23ab96438c329d0a2a0fcc3a435df9
QzarD <a---angel@mail.ru>
2019-10-10 15:05:22 +0700

    add initializedApp

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
 import Content from "./Components/Content/Content";
 import Footer from "./Components/Footer/Footer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
+import {connect} from "react-redux";
+import {withRouter} from "react-router-dom";
+import {compose} from "redux";
+import {initializeApp} from "./Redux/app-reducer";
+import Preloader from "./Components/Common/Preloader/Preloader";
 
-function App(props) {
+class App extends Component {
 
-    return (
+    componentDidMount() {
+        this.props.initializeApp();
+    }
 
-        <div className="app">
-          <header className="app-header">
-            <HeaderContainer/>
-          </header>
-          <div className="navbar">
-            <Navbar/>
-          </div>
-          <div className="content">
-            <Content/>
-          </div>
-          <footer className="footer">
-            <Footer/>
-          </footer>
-        </div>
+    render() {
+        if (!this.props.initialized) { return <Preloader/>}
+        return (
+            <div className="app">
+                <header className="app-header">
+                    <HeaderContainer/>
+                </header>
+                <div className="navbar">
+                    <Navbar/>
+                </div>
+                <div className="content">
+                    <Content/>
+                </div>
+                <footer className="footer">
+                    <Footer/>
+                </footer>
+            </div>
 
-  );
+        );
+    }
 }
 
-export default App;
+const mapStateToProps=(state)=>({
+    initialized: state.app.initialized,
+})
+export default compose(
+    withRouter,
+    connect(mapStateToProps,{initializeApp})
+)(App);
--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -5,6 +5,7 @@ import profileReducer from "./Profiles-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
 import {reducer as formReducer} from "redux-form";
+import appReducer from "./app-reducer";
 
 
 let reducers=combineReducers({
@@ -13,6 +14,7 @@ let reducers=combineReducers({
     profilesPage:profileReducer,
     auth:authReducer,
     form:formReducer,
+    app:appReducer
 });
 
 let store=createStore(reducers, applyMiddleware(thunkMiddleware));
