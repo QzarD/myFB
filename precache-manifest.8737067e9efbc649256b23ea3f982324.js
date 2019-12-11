c0d5409d870429602c990f38001ac38151f98b48
QzarD <a---angel@mail.ru>
2019-12-11 15:08:59 +0700

    Updates

---
+++ precache-manifest.8737067e9efbc649256b23ea3f982324.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9528a0516260407f53b9fd14accaeb39",
+    "url": "/myFB/index.html"
+  },
+  {
+    "revision": "bcda2880e732146fee50",
+    "url": "/myFB/static/css/4.4f22d4d6.chunk.css"
+  },
+  {
+    "revision": "fd4c1ce4f8faafb1084b",
+    "url": "/myFB/static/css/5.3eee3b1e.chunk.css"
+  },
+  {
+    "revision": "9ef31bc1dd53f55b4e79",
+    "url": "/myFB/static/css/6.177d030e.chunk.css"
+  },
+  {
+    "revision": "c41c2a6e6b0b5b24bb72",
+    "url": "/myFB/static/css/7.9fd90bbd.chunk.css"
+  },
+  {
+    "revision": "8644654fa45d6af048a4",
+    "url": "/myFB/static/css/main.f21bdfe5.chunk.css"
+  },
+  {
+    "revision": "5bf0ded0c020db017dd6",
+    "url": "/myFB/static/js/2.44e4262b.chunk.js"
+  },
+  {
+    "revision": "30b87fdf7bd7cf85e6a5",
+    "url": "/myFB/static/js/3.e95bc4b6.chunk.js"
+  },
+  {
+    "revision": "bcda2880e732146fee50",
+    "url": "/myFB/static/js/4.65278d63.chunk.js"
+  },
+  {
+    "revision": "fd4c1ce4f8faafb1084b",
+    "url": "/myFB/static/js/5.b6b2e2f6.chunk.js"
+  },
+  {
+    "revision": "9ef31bc1dd53f55b4e79",
+    "url": "/myFB/static/js/6.a9a21398.chunk.js"
+  },
+  {
+    "revision": "c41c2a6e6b0b5b24bb72",
+    "url": "/myFB/static/js/7.71886455.chunk.js"
+  },
+  {
+    "revision": "8644654fa45d6af048a4",
+    "url": "/myFB/static/js/main.dc4e2fd0.chunk.js"
+  },
+  {
+    "revision": "3e338e5aa3e37a053c55",
+    "url": "/myFB/static/js/runtime~main.222e7028.js"
+  }
+]);
